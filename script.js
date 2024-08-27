b625479bc1109a3fd1c8aff7577dd0326107e3d0
Mithilesh <mithileshpakhare@gmail.com>
2024-08-27 12:36:03 +0530

    intial commit

---
+++ script.js
@@ -0,0 +1,106 @@
+const musicContainer = document.getElementById('music-container');
+const playBtn = document.getElementById('play');
+const prevBtn = document.getElementById('prev');
+const nextBtn = document.getElementById('next');
+const audio = document.getElementById('audio');
+const progress = document.getElementById('progress');
+const progressContainer = document.getElementById('progress-container');
+const title = document.getElementById('title');
+const cover = document.getElementById('cover');
+const volumeControl = document.getElementById('volume-control');
+const playlist = document.getElementById('playlist');
+
+// Song titles
+const songs = ['hey', 'summer', 'ukulele'];
+
+// Load song details into DOM
+function loadSong(song) {
+    title.innerText = song;
+    audio.src = `music/${song}.mp3`;
+    cover.src = `images/${song}.jpg`;
+}
+
+// Play song
+function playSong() {
+    musicContainer.classList.add('play');
+    playBtn.querySelector('i.fas').classList.remove('fa-play');
+    playBtn.querySelector('i.fas').classList.add('fa-pause');
+    audio.play();
+}
+
+// Pause song
+function pauseSong() {
+    musicContainer.classList.remove('play');
+    playBtn.querySelector('i.fas').classList.add('fa-play');
+    playBtn.querySelector('i.fas').classList.remove('fa-pause');
+    audio.pause();
+}
+
+// Previous song
+function prevSong() {
+    songIndex = (songIndex - 1 + songs.length) % songs.length;
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Next song
+function nextSong() {
+    songIndex = (songIndex + 1) % songs.length;
+    loadSong(songs[songIndex]);
+    playSong();
+}
+
+// Update progress bar
+function updateProgress(e) {
+    const { duration, currentTime } = e.srcElement;
+    const progressPercent = (currentTime / duration) * 100;
+    progress.style.width = `${progressPercent}%`;
+}
+
+// Set progress bar
+function setProgress(e) {
+    const width = this.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+    audio.currentTime = (clickX / width) * duration;
+}
+
+// Volume control
+volumeControl.addEventListener('input', (e) => {
+    audio.volume = e.currentTarget.value;
+});
+
+// Load songs into playlist
+function loadPlaylist() {
+    songs.forEach((song, index) => {
+        const li = document.createElement('li');
+        li.innerText = song;
+        li.addEventListener('click', () => {
+            songIndex = index;
+            loadSong(song);
+            playSong();
+        });
+        playlist.appendChild(li);
+    });
+}
+
+// Event listeners
+playBtn.addEventListener('click', () => {
+    const isPlaying = musicContainer.classList.contains('play');
+    if (isPlaying) {
+        pauseSong();
+    } else {
+        playSong();
+    }
+});
+
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
+audio.addEventListener('timeupdate', updateProgress);
+progressContainer.addEventListener('click', setProgress);
+audio.addEventListener('ended', nextSong);
+
+// Initially load song details into DOM
+let songIndex = 2;
+loadSong(songs[songIndex]);
+loadPlaylist();
